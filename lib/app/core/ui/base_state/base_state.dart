c8cf26dd88394ce2813339513b816ac127d7b6b4
luisfelipeas5 <luisfelipeas5@gmail.com>
2023-08-31 21:15:21 -0300

    refactor: separates BasePage in BaseState and StateWithController

--- lib/app/core/ui/base_state/base_state.dart
+++ lib/app/core/ui/base_state/base_state.dart
@@ -1,17 +1,13 @@
+import 'package:app_flutter_arch/app/core/ui/helpers/loader.dart';
+import 'package:app_flutter_arch/app/core/ui/helpers/messages.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../helpers/loader.dart';
-import '../helpers/messages.dart';
-
-abstract class BaseState<T extends StatefulWidget, C extends BlocBase>
-    extends State<T> with Loader, Messages {
-  late final C controller;
+abstract class BaseState<T extends StatefulWidget> extends State<T>
+    with Loader, Messages {
 
   @override
   void initState() {
     super.initState();
-    controller = context.read<C>();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       onReady();
     });
